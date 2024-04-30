a5b0894effcab3bcb7eb6ba9ebcd27e2315d51f5
MainUps <natsubakiy@gmail.com>
2024-04-30 09:25:57 +0900

    two day

--- youtube.py
+++ youtube.py
@@ -1,6 +1,16 @@
 from pytube import YouTube
+import os
 
+link = input("유튜브 URL : ")
+youtube = YouTube(link)
 
+if youtube.age_restricted:
+    print("해당 영상이 연령 제한 있어서 불가능합니다.")
+    
+try:
+    youtube.streams.first().download(os.getcwd())
+    print("성공!")
+except Exception as e:
+    print("에러 :", e)
 
-YouTube(input("URL : ")).streams.first().download(r"C:\Users\UserK\Documents\유튭mp3")
-print("성공!")
+#youtube.streams.filter(only_audio=True).first().download()
