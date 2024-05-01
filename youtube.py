edf7afcf9df629bd6838dc006c3b78de72579fec
MainUps <natsubakiy@gmail.com>
2024-05-01 23:58:07 +0900

    finish !

--- youtube.py
+++ youtube.py
@@ -1,21 +1,19 @@
 from pytube import YouTube
 import os
 
-
 PATH = os.getcwd()
 
-
-while True :
+while True:
     link = input("유튜브 URL (종료 : exit): ")
-    if link == "exit" :
+    if link == "exit":
         break
     
-    youtube = YouTube(link)
-    if youtube.age_restricted:
-        print("해당 영상이 연령 제한 있어서 불가능합니다.")
-        
     try:
-        #youtube.streams.filter(only_audio=True).first().download(PATH)
+        youtube = YouTube(link)
+        if youtube.age_restricted:
+            print("해당 영상이 연령 제한 있어서 불가능합니다.")
+            continue
+        
         audio = youtube.streams.filter(only_audio=True).order_by('abr').last()
         audio.download(output_path=PATH)
         print("저장 성공")
