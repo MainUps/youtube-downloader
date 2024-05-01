e68981940154466a6bf1691e51c22d6b5e7c1d2b
MainUps <natsubakiy@gmail.com>
2024-05-01 23:35:14 +0900

    exit로 종료 구현

--- youtube.py
+++ youtube.py
@@ -1,15 +1,23 @@
 from pytube import YouTube
 import os
 
-link = input("유튜브 URL : ")
-youtube = YouTube(link)
+
 PATH = os.getcwd()
 
-if youtube.age_restricted:
-    print("해당 영상이 연령 제한 있어서 불가능합니다.")
+
+while True :
+    link = input("유튜브 URL (종료 : exit): ")
+    if link == "exit" :
+        break
     
-try:
-    youtube.streams.filter(only_audio=True).first().download(PATH)
-    print("저장 성공")
-except Exception as e:
-    print("에러 :", e)
+    youtube = YouTube(link)
+    if youtube.age_restricted:
+        print("해당 영상이 연령 제한 있어서 불가능합니다.")
+        
+    try:
+        #youtube.streams.filter(only_audio=True).first().download(PATH)
+        audio = youtube.streams.filter(only_audio=True).order_by('abr').last()
+        audio.download(output_path=PATH)
+        print("저장 성공")
+    except Exception as e:
+        print("에러 :", e)
