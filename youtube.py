2db8a2460184a334ea7876f58304d49311e908e0
MainUps <natsubakiy@gmail.com>
2024-05-01 23:02:56 +0900

    change

--- youtube.py
+++ youtube.py
@@ -3,13 +3,13 @@ import os
 
 link = input("유튜브 URL : ")
 youtube = YouTube(link)
+PATH = os.getcwd()
 
 if youtube.age_restricted:
     print("해당 영상이 연령 제한 있어서 불가능합니다.")
     
 try:
-    youtube.streams.filter(only_audio=True).first().download(os.getcwd())
-    print("성공!")
+    youtube.streams.filter(only_audio=True).first().download(PATH)
+    print("저장 성공")
 except Exception as e:
     print("에러 :", e)
-
